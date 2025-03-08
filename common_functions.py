d5f884f97a754f26eef052b9f8f3ec18b0400941
Pietro2302 <pietromassari@icloud.com>
2025-03-08 12:47:44 +0100

    Changed common_functions for linting

--- common_functions.py
+++ common_functions.py
@@ -1,13 +1,15 @@
 import json
 
+
 def load_characters(filename):
-    try: 
-        with open(filename,"r") as json_file:
+    try:
+        with open(filename, "r") as json_file:
             type(json_file)
             return json.load(json_file)
     except FileNotFoundError:
         return []
-    
+
+
 def save_characters(filename, characters_dict):
     with open(filename, "w") as json_file:
         json.dump(characters_dict, json_file, indent=4)
